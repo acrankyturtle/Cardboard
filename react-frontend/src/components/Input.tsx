24c05ea4d5b73bd63887c3e9e35a0abf07e319c8
cranky <crankyturtle@hotmail.com>
2026-02-26 22:49:10 -0500

    remove unused Input component

--- react-frontend/src/components/Input.tsx
+++ react-frontend/src/components/Input.tsx
@@ -1,12 +1,4 @@
-import { Input as HeadlessInput } from "@headlessui/react";
 import clsx, { ClassValue } from "clsx";
-import { ComponentPropsWithRef } from "react";
-
-export function Input({
-  className,
-}: ComponentPropsWithRef<typeof HeadlessInput>) {
-  return <HeadlessInput className={clsx(InputClassName, className)} />;
-}
 
 const BaseClassName: ClassValue =
   "rounded-md border-1 shadow-sm outline-0 focus-within:ring-1 focus-within:ring-violet-500 px-2 py-1 data-disabled:opacity-50";
